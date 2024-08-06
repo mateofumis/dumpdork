ce68981eb961a246340e2154a24a0e661a514c50
mateofumis <87695536+mateofumis@users.noreply.github.com>
2024-08-06 01:18:22 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dumpdork',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'colorama==0.4.6',
+        'PyYAML==6.0.1',
+        'requests==2.32.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dumpdork=dumpdork:main',
+        ],
+    },
+    description='A powerful command-line tool for Google dorking, enabling users to uncover hidden information and vulnerabilities with advanced search queries.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mateofumis/dumpdork',
+    author='Mateo Fumis',
+    author_email='mateofumis1@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache license 2.0',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
